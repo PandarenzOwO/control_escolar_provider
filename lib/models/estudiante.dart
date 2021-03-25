ddb60ec4a562192816239a0a856cae35876f254c
Fabian Cordero <psychopathpandaren@gmail.com>
2021-03-24 23:57:22 -0600

    agregado modelo estudiante

---
+++ lib/models/estudiante.dart
@@ -0,0 +1,23 @@
+class Estudiante {
+  int id;
+  String nombre;
+  String apepat;
+  String apemat;
+  int edad;
+  int grado;
+  String grupo;
+  int idprofesor;
+  String foto;
+
+  Estudiante({
+    this.id,
+    this.nombre,
+    this.apepat,
+    this.apemat,
+    this.edad,
+    this.grado,
+    this.grupo,
+    this.idprofesor,
+    this.foto,
+  });
+}
