1e81bc15196ed31a1f30b47f26ff517e747fe06c
Fabian Cordero <psychopathpandaren@gmail.com>
2021-04-01 22:23:47 -0600

    Se agregaron rutas y formato para cards navegation

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -33,9 +33,17 @@ class _HomePageState extends State<HomePage> {
             children: [
               Column(
                 children: [
-                  Text("Carta agregar alumno"),
-                  Text("Carta lista de alumnos"),
-                  Text("Carta alumnos graduados"),
+                  navegadorCartas(
+                      context,
+                      "Agregar Alumnos",
+                      "https://img.icons8.com/dusk/452/add-user-group-man-woman.png",
+                      "Aqui podra agregar nuevos alumnos a la escuela",
+                      "agregar"),
+                  navegadorCartas(context, "titulo", "imagen", "texto", "ruta"),
+                  navegadorCartas(context, "titulo", "imagen", "texto", "ruta"),
+                  // Text("Carta agregar alumno"),
+                  // Text("Carta lista de alumnos"),
+                  // Text("Carta alumnos graduados"),
                 ],
               ),
             ],
@@ -45,10 +53,47 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  Widget cartas() {
-    return Padding(
-      padding: EdgeInsets.all(10.0),
-      child: Card(),
+  Widget navegadorCartas(BuildContext context, String titulo, String imagen,
+      String texto, String ruta) {
+    return InkWell(
+      onTap: () {
+        Navigator.pushNamed(context, ruta);
+      },
+      child: Card(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+        margin: EdgeInsets.all(15.0),
+        elevation: 10.0,
+        child: Row(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Image.network(
+                imagen,
+                height: 100,
+                width: 100,
+                fit: BoxFit.cover,
+              ),
+            ),
+            Expanded(
+                child: Column(
+              children: [
+                Text(
+                  titulo,
+                  style: TextStyle(
+                    fontSize: 24.0,
+                    fontWeight: FontWeight.w400,
+                    fontStyle: FontStyle.italic,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 10.0, right: 10.0),
+                  child: Text("\n" + texto, textAlign: TextAlign.center),
+                ),
+              ],
+            ))
+          ],
+        ),
+      ),
     );
   }
 }
