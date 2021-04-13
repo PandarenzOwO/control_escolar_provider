93829908bd33df02f8c879a0cd28a07dbb698ddf
Fabian Cordero <psychopathpandaren@gmail.com>
2021-04-12 21:19:21 -0500

    Se corregio el error con el provider en main

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,27 @@
 import 'package:control_escolar_provider/home_page.dart';
 import 'package:control_escolar_provider/provider/agregar_page_provider.dart';
+import 'package:control_escolar_provider/provider/alumnos_provider.dart';
 import 'package:control_escolar_provider/provider/bajas_page_provider.dart';
 import 'package:control_escolar_provider/provider/lista_page_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        initialRoute: '/',
-        routes: {
-          '/': (context) => HomePage(),
-          'lista': (context) => ListaPage(),
-          'bajas': (context) => BajasPage(),
-          'agregar': (context) => AgregarEstudiantePage(),
-        });
+    return ChangeNotifierProvider(
+      create: (context) => EstudiantesProvider(),
+      child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          initialRoute: '/',
+          routes: {
+            '/': (context) => HomePage(),
+            'lista': (context) => ListaPage(),
+            'bajas': (context) => BajasPage(),
+            'agregar': (context) => AgregarEstudiantePage(),
+          }),
+    );
   }
 }
