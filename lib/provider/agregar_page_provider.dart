c3b2e41a308f62fa016a1dae33eaa27909cda553
Diego <g0d13@outlook.com>
2021-04-12 22:16:39 -0500

    actualizar agregar_page_provider

--- lib/provider/agregar_page_provider.dart
+++ lib/provider/agregar_page_provider.dart
@@ -35,113 +35,41 @@ class _FormExampleState extends State<AgregarPage> {
   TextEditingController txtedad = TextEditingController();
   TextEditingController txtfoto = TextEditingController();
 
+  Widget generateInput(controller, validator, hintText) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 20.0),
+      child: TextFormField(
+        controller: controller,
+        validator: (val) => validator(val),
+        decoration: InputDecoration( labelText: hintText),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         SizedBox(height: 10.0),
-        // Center(
-        //   child: Text(
-        //     "Ingrese los datos del alumno",
-        //     style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w400),
-        //   ),
-        // ),
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: 20.0),
-          child: TextFormField(
-            controller: txtnombre,
-            validator: (value) {
-              if (value.isEmpty) {
-                return 'Por favor, ingresa un nombre';
-              } else {
-                return null;
-              }
-            },
-            decoration: InputDecoration(
-                hintText: "Nombre", labelText: "Ingrese el nombre del alumno"),
-          ),
-        ),
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: 20.0),
-          child: TextFormField(
-            controller: txtapellidos,
-            validator: (value) {
-              if (value.isEmpty) {
-                return 'Por favor, ingrese los apellidos';
-              } else {
-                return null;
-              }
-            },
-            decoration: InputDecoration(
-                hintText: "Apellidos",
-                labelText: "Ingrese los apellidos del alumno"),
-          ),
-        ),
-        SizedBox(height: 15),
+        generateInput(txtnombre, (String text) => text.isEmpty, "Ingresa el nombre"),
+        generateInput(txtgrado, (String text) => text.isEmpty, "Ingresa los grados xd"),
+        generateInput(txtgrupo, (String text) => text.isEmpty, "Ingresa el grupo"),
+        generateInput(txtedad, (String text) => text.isEmpty, "Ingresa la edad"),
+        generateInput(txtfoto, (String text) => text.isEmpty, "Ingresa la foto"),
+        SizedBox(height: 10,),
         Row(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            SizedBox(
-              width: 20,
-              height: 20,
+            TextButton(
+              onPressed: () {},
+              child: Row(
+                children: [Icon(Icons.add), Text("Popo")],
+              ),
             ),
-            Text("Semestre: "),
-            //MyStatefulWidget(),
           ],
         ),
       ],
     );
   }
 }
-
-//////////////////////***************************//////////////////////
-// class MyStatefulWidget extends StatefulWidget {
-//   const MyStatefulWidget({Key key}) : super(key: key);
-
-//   @override
-//   _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
-// }
-
-// /// This is the private State class that goes with MyStatefulWidget.
-// class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-//   String dropdownValue = 'Primero';
-//   List<String> valores = <String>[
-//     'Primero',
-//     'Segundo',
-//     'Tercero',
-//     'Cuarto',
-//     'Quinto',
-//     'Sexto',
-//     'Septimo',
-//     'Octavo',
-//     'Noveno',
-//     'Decimo',
-//   ];
-//   @override
-//   Widget build(BuildContext context) {
-//     return DropdownButton<String>(
-//       value: dropdownValue,
-//       icon: const Icon(Icons.keyboard_arrow_down_rounded),
-//       iconSize: 24,
-//       elevation: 8,
-//       style: const TextStyle(color: Colors.black),
-//       underline: Container(
-//         height: 1,
-//         color: Colors.blueAccent,
-//       ),
-//       onChanged: (String newValue) {
-//         setState(() {
-//           dropdownValue = newValue;
-//         });
-//       },
-//       items: valores.map<DropdownMenuItem<String>>((String value) {
-//         return DropdownMenuItem<String>(
-//           value: value,
-//           child: Text(value),
-//         );
-//       }).toList(),
-//     );
-//   }
-// }
-
-//////////////////////***************************//////////////////////
